f1020216dc83d976aef8fb6cde133334124f374a
dany <stefanusdany12@gmail.com>
2022-06-11 21:28:19 +0700

    adding fragment detail map

--- app/src/main/java/com/bangkit/lokasee/data/retrofit/ApiConfig.kt
+++ app/src/main/java/com/bangkit/lokasee/data/retrofit/ApiConfig.kt
@@ -1,10 +1,5 @@
 package com.bangkit.lokasee.data.retrofit
 
-import android.content.Context
-import androidx.datastore.core.DataStore
-import androidx.datastore.preferences.core.Preferences
-import androidx.datastore.preferences.preferencesDataStore
-import com.bangkit.lokasee.data.AppPreferences
 import com.bangkit.lokasee.data.store.UserStore.currentUserToken
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
@@ -17,8 +12,9 @@ object ApiConfig {
     const val HOST: String = "http://34.77.53.105"
 
     fun getApiService(): ApiService {
-        val loggingInterceptor = HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
-        val client: OkHttpClient = if(currentUserToken != ""){
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+        val client: OkHttpClient = if (currentUserToken != "") {
             OkHttpClient.Builder().addInterceptor(Interceptor { chain ->
                 val newRequest: Request = chain.request().newBuilder()
                     .addHeader("Authorization", "Bearer $currentUserToken")
--- app/src/main/java/com/bangkit/lokasee/ui/main/post/PostFragment.kt
+++ app/src/main/java/com/bangkit/lokasee/ui/main/post/PostFragment.kt
@@ -1,32 +1,29 @@
 package com.bangkit.lokasee.ui.main.post
 
-import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.transition.Slide
 import com.bangkit.lokasee.R
 import com.bangkit.lokasee.data.Post
 import com.bangkit.lokasee.data.retrofit.ApiConfig
 import com.bangkit.lokasee.databinding.FragmentPostBinding
-import com.bangkit.lokasee.ui.main.MainViewModel
-import com.bangkit.lokasee.util.getAvatarUrl
-import com.bangkit.lokasee.util.themeColor
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
-import com.google.android.material.transition.MaterialContainerTransform
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.material.transition.MaterialFadeThrough
-import com.google.android.material.transition.MaterialSharedAxis
 import org.imaginativeworld.whynotimagecarousel.model.CarouselItem
 
-class PostFragment : Fragment() {
+class PostFragment : Fragment(), OnMapReadyCallback {
     private lateinit var post: Post
     private var _binding: FragmentPostBinding? = null
     private val binding get() = _binding!!
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +36,7 @@ class PostFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         post = arguments?.getParcelable("POST")!!
         _binding = FragmentPostBinding.inflate(inflater, container, false)
         return binding.root
@@ -47,11 +44,13 @@ class PostFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setupMapView()
+
         val imageUrlList = mutableListOf<CarouselItem>()
         post.images.forEach {
             imageUrlList.add(CarouselItem(ApiConfig.HOST + it))
         }
-        with(binding){
+        with(binding) {
             carouselPost.setData(imageUrlList)
             txtPostTitle.text = post.title
             txtPostDesc.text = post.desc
@@ -65,5 +64,29 @@ class PostFragment : Fragment() {
 
             }
         }
+    }
+
+    private fun setupMapView() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        mMap.uiSettings.isZoomControlsEnabled = true
+        mMap.uiSettings.isIndoorLevelPickerEnabled = true
+        mMap.uiSettings.isCompassEnabled = true
+        mMap.uiSettings.isMapToolbarEnabled = true
+
+        // TODO ADD LAT LONG OF POST
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(
+                sydney,
+                5f
+            )
+        )
     }
 }
